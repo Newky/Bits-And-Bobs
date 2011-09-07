664b0d349e737b52b8b30f42b5824399951d14f8
Richy Delaney(cube) <richdel1991@gmail.com>
2011-09-07 10:04:06 +0100

    Added a slight modification, it should grep for /tmp/Flash not just Flash for total accuracy :)
    Thanks to ahmed kotb for this spot :)

--- downloadFlashFiles.sh
+++ downloadFlashFiles.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lsof | grep Flash | awk 'BEGIN { FS=" "; print "#!/bin/bash"} { FILE=substr($4,1,length($4-1)); print "cp /proc/" $2 "/fd/" FILE " ~/Videos/"FILE".flv"  }' | sh
+lsof | grep /tmp/Flash | awk 'BEGIN { FS=" "; print "#!/bin/bash"} { FILE=substr($4,1,length($4-1)); print "cp /proc/" $2 "/fd/" FILE " ~/Videos/"FILE".flv"  }' | sh
