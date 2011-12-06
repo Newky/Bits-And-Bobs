dca16d85faa95d467ba4a8663767160ab435aafa
Newky <richdel1991@gmail.com>
2011-12-06 21:01:18 +0000

    Slight modification to xmas.hs Don't need spaces after the character obviously.

--- xmas.hs
+++ xmas.hs
@@ -1,6 +1,6 @@
 r=replicate
 t c 0 _=[]
-t c s o=((r s ' ')++(r o c)++(r s ' ')):t 'o' (s-1) (o+2)
+t c s o=((r s ' ')++(r o c)):t 'o' (s-1) (o+2)
 m = putStrLn $ (unlines) $(t '*' 10 1)
 
 -- produces:
