54695e3d803f39a0838acd4f2e28ea9c50fa3b90
Newky <richdel1991@gmail.com>
2013-11-10 22:49:16 +0000

    Added script which I believe (only partly tested)
    will allow me to keep my screen from going idle when watching movies.

---
+++ movie_keep_alive.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+while true ; do
+    RANDOM=`date +%N|sed s/...$//`
+    random_x=$(( ( RANDOM % 1000 )  + 1 ));
+    random_y=$(( ( RANDOM % 1000 )  + 1 ));
+    echo "moved the mouse to $random_x and $random_y"
+    xdotool mousemove $(( $random_x )) $(( $random_y ));
+    sleep $(( 5 * 60 ));
+done
