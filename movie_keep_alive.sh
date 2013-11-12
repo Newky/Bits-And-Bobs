9da5ecc70c07087eb70712c8cb2e09234f469628
Newky <richdel1991@gmail.com>
2013-11-12 23:27:53 +0000

    Changed movie_keep_alive.sh to be a bash script.
    This stops problems with the random number generator

--- movie_keep_alive.sh
+++ movie_keep_alive.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 while true ; do
-    RANDOM=`date +%N|sed s/...$//`
     random_x=$(( ( RANDOM % 1000 )  + 1 ));
     random_y=$(( ( RANDOM % 1000 )  + 1 ));
     echo "moved the mouse to $random_x and $random_y"
