eee039485556c9e723cde2379b2c24ceffd81bff
Newky <richdel1991@gmail.com>
2013-12-10 22:00:18 +0000

    Some improvements to download flash files and movie keep alive.
    Put a limit on movie_keep_alive.sh
    Also made a print statement and slightly speeded up downloadFlashfiles

--- downloadFlashFiles.sh
+++ downloadFlashFiles.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lsof | grep /tmp/Flash | awk 'BEGIN { FS=" "; print "#!/bin/bash"} { FILE=substr($4,1,length($4-1)); print "cp /proc/" $2 "/fd/" FILE " ~/Videos/"FILE".flv"  }' | sh
+lsof -nP | grep /tmp/Flash | awk 'BEGIN { FS=" "; print "#!/bin/bash"} { FILE=substr($4,1,length($4-1)); DEST="~/Videos/"FILE".flv"; print "cp /proc/" $2 "/fd/" FILE " " DEST; print "echo copied to "DEST }' | sh
--- movie_keep_alive.sh
+++ movie_keep_alive.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+count=0
+limit=12
+
 while true ; do
+    count=$(( count + 1 ));
     random_x=$(( ( RANDOM % 1000 )  + 1 ));
     random_y=$(( ( RANDOM % 1000 )  + 1 ));
     echo "moved the mouse to $random_x and $random_y"
     xdotool mousemove $(( $random_x )) $(( $random_y ));
     sleep $(( 5 * 60 ));
+    if [ $count -eq $limit ]; then
+        break;
+    fi
 done
