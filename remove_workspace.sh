b7a43a9bea5775295f8221a38b721ab95bb42612
Newky <richdel1991@gmail.com>
2014-01-02 10:49:11 +0000

    Fixing up some log messages

--- remove_workspace.sh
+++ remove_workspace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 WORKSPACES=$(gsettings get org.mate.Marco.general num-workspaces)
 echo "currently $WORKSPACES workspaces"
-echo "Adding one, so now there will be $(( WORKSPACES - 1 ))"
+echo "Removing one, so now there will be $(( WORKSPACES - 1 ))"
 gsettings set org.mate.Marco.general num-workspaces $(( WORKSPACES - 1))
